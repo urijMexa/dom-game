e124ff601ae1b1bd6dd2abed18904d5ca9f36dc4
Yuri Blinnikov <blinnikovu5@gmail.com>
2025-08-30 13:52:14 +0800

    ref

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,44 +1,45 @@
 import goblinImg from '../img/goblin.png';
 
 export default class Game {
-    constructor() {
-        this.boardSize = 4;
-        this.gameContainer = document.getElementById('game-container');
-        this.cells = [];
-        this.character = null;
-        this.currentPosition = -1;
+  constructor() {
+    this.boardSize = 4;
+    this.gameContainer = document.getElementById('game-container');
+    this.cells = [];
+    this.character = null;
+    this.currentPosition = -1;
+  }
+
+  init() {
+    this.drawBoard();
+    this.createCharacter();
+    setInterval(() => this.moveCharacter(), 1000);
+  }
+
+  drawBoard() {
+    for (let i = 0; i < this.boardSize ** 2; i += 1) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      this.gameContainer.append(cell);
+      this.cells.push(cell);
     }
+  }
 
-    init() {
-        this.drawBoard();
-        this.createCharacter();
-        setInterval(() => this.moveCharacter(), 1000);
-    }
+  createCharacter() {
+    const characterEl = document.createElement('img');
+    characterEl.src = goblinImg;
+    characterEl.classList.add('goblin');
+    characterEl.alt = 'Goblin character';
+    this.character = characterEl;
+  }
 
-    drawBoard() {
-        for (let i = 0; i < this.boardSize ** 2; i++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            this.gameContainer.appendChild(cell);
-            this.cells.push(cell);
-        }
-    }
+  moveCharacter() {
+    let newPosition;
 
-    createCharacter() {
-        const characterEl = document.createElement('img');
-        characterEl.src = goblinImg;
-        characterEl.classList.add('goblin');
-        this.character = characterEl;
-    }
+    do {
+      newPosition = Math.floor(Math.random() * this.cells.length);
+    } while (newPosition === this.currentPosition);
 
-    moveCharacter() {
-        let newPosition;
-
-        do {
-            newPosition = Math.floor(Math.random() * this.cells.length);
-        } while (newPosition === this.currentPosition);
-
-        this.currentPosition = newPosition;
-        this.cells[this.currentPosition].appendChild(this.character);
-    }
+    this.currentPosition = newPosition;
+    this.cells[this.currentPosition].append(this.character);
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import '../css/style.css';
 import Game from './Game';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const game = new Game();
-    game.init();
+  const game = new Game();
+  game.init();
 });
